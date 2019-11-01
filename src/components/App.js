611b45ef1ba3c495e25a58dc3102984e16fe976d
sungwoo1548 <swo1548@gmail.com>
2019-11-01 11:42:50 +0900

    set style

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 
-import Loader from './Loader'
+import Loader from './Loader';
 import SearchSection from './SearchSection';
 import VeiwSection from './VeiwSection';
+
+import "./App.css";
 
 export default class App extends Component {
     render() {
         return (
-            <div className="Container">
-                <SearchSection />
-                <VeiwSection />
-                <Loader />
+            <div className="container">
+                <div className="container search">
+                    <SearchSection />
+                </div>
+                <div className="container view">
+                    <VeiwSection />
+                </div>
+                {/* <div className="container load">
+                    <Loader />
+                </div> */}
             </div>
         )
     }
--- src/components/VeiwSection.js
+++ src/components/VeiwSection.js
@@ -3,7 +3,7 @@ import React from 'react'
 const VeiwSection = () => {
     return (
         <div>
-            Viwe
+            View
         </div>
     )
 }
